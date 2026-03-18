3ecda7fd36128f2d0ab3c7046f3f6bab2aa6e646
Overscore <jwebster@concordia.sa.edu.au>
2026-03-18 12:12:37 +1030

    a

---
+++ shrine.js
@@ -0,0 +1,29 @@
+const silverLink = document.getElementById("silverShrine");
+
+let currentState = false;
+let animationId = null;
+
+function shrineEnter() {
+    // const containerRect = container.getBoundingClientRect();
+    const ballRect = ball.getBoundingClientRect();
+}
+
+function enter() {
+    console.log("hi :)");
+    if(currentState == true) {
+        exit();
+    }else{
+        currentState = true;
+        if (!animationId) {
+            animationId = requestAnimationFrame(shrineEnter);
+
+        }
+    }
+}
+
+function exit() {
+    cancelAnimationFrame(animationId);
+    animationId = null;
+}
+
+document.getElementById("theShrine").addEventListener('click', enter());
