a9e5788f4a110d89df41860fa9e3fadb1704752c
Overscore <jwebster@concordia.sa.edu.au>
2026-03-19 19:32:47 +1030

    a

--- js/switch.js
+++ js/switch.js
@@ -1,3 +1,8 @@
 const aboutBut = document.getElementById("about");
 const projectsBut = document.getElementById("projects");
 const friendsBut = document.getElementById("friends");
+
+document.getElementById("about").addEventListener('click', enter());
+document.getElementById("projects").addEventListener('click', enter());
+document.getElementById("friends").addEventListener('click', enter());
+
