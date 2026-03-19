7b6c19c6804b58833d0344bb78ac1ae9d2155764
Overscore <Overscore34@users.noreply.github.com>
2026-03-19 19:47:32 +1030

    a

--- js/switch.js
+++ js/switch.js
@@ -2,6 +2,11 @@ const aboutBut = document.getElementById("about");
 const projectsBut = document.getElementById("projects");
 const friendsBut = document.getElementById("friends");
 
-document.getElementById("about").addEventListener('click', enter());
-document.getElementById("projects").addEventListener('click', enter());
-document.getElementById("friends").addEventListener('click', enter());
+// document.getElementById("about").addEventListener('click', aboutPage);
+// document.getElementById("projects").addEventListener('click', enter());
+document.getElementById("friends").addEventListener('click', friendsPage);
+
+function friendsPage() {
+    console.log("now");
+    window.location.href = "friends.html";
+}
